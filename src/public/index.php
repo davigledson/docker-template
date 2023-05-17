17c962cb2026b986826931f46b3bbea4630e3335
Davi Gledson <davigledson@outlook.com>
2023-05-17 11:56:31 -0300

    nova aula

--- src/public/index.php
+++ src/public/index.php
@@ -1,114 +1,165 @@
+
+
 <?php
-declare(strict_types=1);
-phpinfo();
-echo 'Olá mundo';
+// declare(strict_types=1);
+// phpinfo();
+// echo 'Olá mundo';
 
- $contador = 1;
+//  $contador = 1;
 
- $mensagem = "olá";
- $versao = array('1.1','2','3.4');
+//  $mensagem = "olá";
+//  $versao = array('1.1','2','3.4');
 
- print_r($versao);
-
-
- $primeiro_nome = "Maria";
- echo $primeiro_nome;
- echo '<hr>';
- $ultimo_nome = "lourdesddd";
- echo $ultimo_nome;
-echo '<hr>';
- $time = array('maria','jose','ana');
-
- echo $time[2];
- echo '<br>';
- print_r($time);
- echo '<hr>';
- echo 'a: [' . true . '] <br/>';
- echo 'a: [' . false . '] <br/>';
-
- $idade = 20;
- echo 'a: [' . (20>9) . '] <br/>';
- echo 'b: [' . (5 == 6) . '] <br/>';
- echo 'c: [' . (1==0) . '] <br/>';
- echo 'd: [' . (1 === '1') . '] <br/>';
- echo 'd: [' . "$idade" . '] <br/>';
- echo '<hr>';
-$saldo = 10;
-if($saldo<100){
-    $deposito = 1000;
-    $saldo += $deposito;
-
-}
-
-echo $saldo;
-echo '<hr>';
-
-$menu = 'sobre';
-switch ($menu) {
-    case 'index';
-        echo 'Escolheu a página Inicial';
-        break;
-
-    case 'sobre':
-        echo 'Escolheu a página Sobre';
-        break;
-
-    case 'novidades':
-        echo 'Escolheu a página Novidades';
-        break;
-    case 'links':
-        echo 'Escolheu a página Links';
-        break;
-
-    default:
-        echo 'Pagina não encontrada';
-        break;
-}
+//  print_r($versao);
 
 
+//  $primeiro_nome = "Maria";
+//  echo $primeiro_nome;
+//  echo '<hr>';
+//  $ultimo_nome = "lourdesddd";
+//  echo $ultimo_nome;
+// echo '<hr>';
+//  $time = array('maria','jose','ana');
 
-$tanque =1;
-$tanque <= 1 ? 'Abastecer agora <br/>' : 'Combustivel OK <br/>';
-$statusTanque = $tanque <= 1 ? FALSE : TRUE;
+//  echo $time[2];
+//  echo '<br>';
+//  print_r($time);
+//  echo '<hr>';
+//  echo 'a: [' . true . '] <br/>';
+//  echo 'a: [' . false . '] <br/>';
 
-if($statusTanque) echo 'Combustível OK';
+//  $idade = 20;
+//  echo 'a: [' . (20>9) . '] <br/>';
+//  echo 'b: [' . (5 == 6) . '] <br/>';
+//  echo 'c: [' . (1==0) . '] <br/>';
+//  echo 'd: [' . (1 === '1') . '] <br/>';
+//  echo 'd: [' . "$idade" . '] <br/>';
+//  echo '<hr>';
+// $saldo = 10;
+// if($saldo<100){
+//     $deposito = 1000;
+//     $saldo += $deposito;
 
-else echo 'Procurar um posto para abastecer.';
+// }
 
-$cout = 1;
-while ($cout<=10){
-    echo $cout . ' x 10 = ' . $cout *10 . '<br />';
-    ++$cout;
-}
-$cout = 1;
-echo '<br />';
+// echo $saldo;
+// echo '<hr>';
+
+// $menu = 'sobre';
+// switch ($menu) {
+//     case 'index';
+//         echo 'Escolheu a página Inicial';
+//         break;
+
+//     case 'sobre':
+//         echo 'Escolheu a página Sobre';
+//         break;
+
+//     case 'novidades':
+//         echo 'Escolheu a página Novidades';
+//         break;
+//     case 'links':
+//         echo 'Escolheu a página Links';
+//         break;
+
+//     default:
+//         echo 'Pagina não encontrada';
+//         break;
+// }
 
 
- $vegetais = array('jerimum','batata','tomate');
-$vegetais[20] = 'melancia';
 
-echo 'Hoje vou cozinhar ' . $vegetais[0] . '<br/>';
-echo 'Hoje vou cozinhar ' . $vegetais[1] . '<br/>';
-echo 'Hoje vou cozinhar ' . $vegetais[2] . '<br/>';
-echo 'Hoje vou cozinhar ' . $vegetais[20] . '<br/>';
+// $tanque =1;
+// $tanque <= 1 ? 'Abastecer agora <br/>' : 'Combustivel OK <br/>';
+// $statusTanque = $tanque <= 1 ? FALSE : TRUE;
 
-echo 'Vegetais disponiveis ' . count($vegetais) . '<br/>';
-print_r($vegetais);
-echo ' <br/>';
-var_dump($vegetais);
+// if($statusTanque) echo 'Combustível OK';
+
+// else echo 'Procurar um posto para abastecer.';
+
+// $cout = 1;
+// while ($cout<=10){
+//     echo $cout . ' x 10 = ' . $cout *10 . '<br />';
+//     ++$cout;
+// }
+// $cout = 1;
+// echo '<br />';
 
 
-$total = count($vegetais);
-for($i=0;$i<$total; $i++){
-    echo 'Hoje vou cozinhar ' . $vegetais[$i] . '<br />';
-}
+//  $vegetais = array('jerimum','batata','tomate');
+// $vegetais[20] = 'melancia';
 
-$vegetais = array(
-    'jerimum'=>'https://cdn-icons-png.flaticon.com/512/1261/1261113.png',
-    'batata'=>'https://i.pinimg.com/originals/d2/f1/21/d2f12195eb8f743b338f8df854926f98.png',
-    'tomate'=>'https://cdn-icons-png.flaticon.com/512/1486/1486246.png'
-);
+// echo 'Hoje vou cozinhar ' . $vegetais[0] . '<br/>';
+// echo 'Hoje vou cozinhar ' . $vegetais[1] . '<br/>';
+// echo 'Hoje vou cozinhar ' . $vegetais[2] . '<br/>';
+// echo 'Hoje vou cozinhar ' . $vegetais[20] . '<br/>';
 
-foreach($vegetais as $chave =>$valor){
-    echo 'Hoje vou cozinhar ' . $chave . "<img src=\"$valor\" /> < br/>";
-}
+// echo 'Vegetais disponiveis ' . count($vegetais) . '<br/>';
+// print_r($vegetais);
+// echo ' <br/>';
+// var_dump($vegetais);
+
+
+// $total = count($vegetais);
+// for($i=0;$i<$total; $i++){
+//     echo 'Hoje vou cozinhar ' . $vegetais[$i] . '<br />';
+// }
+
+// $vegetais = array(
+//     'jerimum'=>'https://cdn-icons-png.flaticon.com/512/1261/1261113.png',
+//     'batata'=>'https://i.pinimg.com/originals/d2/f1/21/d2f12195eb8f743b338f8df854926f98.png',
+//     'tomate'=>'https://cdn-icons-png.flaticon.com/512/1486/1486246.png'
+// );
+
+// foreach($vegetais as $chave =>$valor){
+//     echo 'Hoje vou cozinhar ' . $chave . "<img src=\"$valor\" /> < br/>";
+// }
+
+// $estoqueCarros = array(
+//     array('Palio',10,125),
+//     array('gol',10,200),
+//     array('Civic',4,38)
+// );
+
+// echo 'Temos' . $estoqueCarros[0][1] . ' ' . $estoqueCarros[0][0] . '<br /> Vendidos' . $estoqueCarros[0][2] .' ';
+
+
+// print_r([$GLOBALS]);
+// echo '<hr/>';
+// print_r([$_SERVER]);
+//print_r([$_GET]);
+// if (empty($_GET['nome'])){
+//     echo 'Nome é um campo obrigatório';
+// } else {
+//     echo $_GET['nome'] . '  obrigado por se inscrever';
+// }
+date_default_timezone_set('america/sao_paulo');
+
+
+
+// $data = date('d/m/Y');
+
+// $hora = date('H');
+
+// if ($hora < 12){
+//     $mensagem = " Bom dia! <img/>";
+// } elseif(date('H') > 11 && date('H') < 18) {
+//     $mensagem = "Boa tarde! <img/>";
+//     $mensagem.= ' Aceita um chá gelado?';
+// } elseif (date('H') > 17){
+//     $mensagem = 'Boa noite!';
+// }
+// echo 'ola' . $mensagem;
+//phpinfo();
+
+
+?>
+
+<!-- <form action="" method="get" action='index.php'>
+    nome: <input type="text" name="nome"/>
+    <input type="submit" value="enviar">
+</form> -->
+
+
+
+
