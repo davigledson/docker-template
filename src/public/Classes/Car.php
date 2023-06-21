cd7158116d674c0f78ea73f9f6a33bc0de1d45ef
Davi Gledson <davigledson@outlook.com>
2023-06-21 11:54:59 -0300

    classe pessoa

--- src/public/Classes/Car.php
+++ src/public/Classes/Car.php
@@ -38,3 +38,25 @@ echo $car2->marca;
 $car1->desligar();
 
 $car2->acelerar();
+echo '<br>';
+
+class Pessoa {
+    public $nome;
+    public $idade;
+
+
+    function __construct() {
+        echo 'Um objeto foi criado <br/>';  
+    }
+
+    function perfil(){
+        echo 'Eu sou'. $this->nome. ' <br/>';
+        echo 'Eu tenho'. $this->idade. 'anos de idade <br/>';
+    }
+    
+}
+
+$p1 = new Pessoa();
+$p1->nome = 'maria';
+$p1->idade = 22;
+$p1->perfil();
